aa5bffee948cf02a254c1c1066f43fd6e6cd3353
Sandulescu Razvan Alexandru <sandulescu.razvan.alexandru@gmail.com>
2022-12-15 22:45:29 +0200

    Classes & Tests

---
+++ src/Addition.java
@@ -0,0 +1,5 @@
+public class Addition {
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    private String s;
+    public Hello(String s) {
+        this.s = s;
+    }
+    public String hello() {
+        return "Hello " + s;
+    }
+}
